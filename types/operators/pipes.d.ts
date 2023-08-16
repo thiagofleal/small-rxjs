de6d4bf66aa231768cd6a026dff79ccc841eaa6d
Thiago Fernandes Leal <thiagofernandesleal@yahoo.com.br>
2023-08-15 21:54:09 -0300

    fix : Types

--- types/operators/pipes.d.ts
+++ types/operators/pipes.d.ts
@@ -1,18 +1,14 @@
-import { Observable } from "../observable";
+import { UnaryFunction } from "../observable";
 
-interface UnaryFunction<T, R> {
-  (source: T): R
-}
-
-export function map<T, R>(transform: UnaryFunction<T, R>): Observable<R>
-export function filter<T, R>(condition: UnaryFunction<T, R>): Observable<R>;
-export function first<T>(): Observable<T>;
-export function timeout<T>(time: number): Observable<T>;
-export function keepAlive<T>(time: number): Observable<T>;
-export function retry<T>(count: number): Observable<T>;
+export function map<T, R>(transform: UnaryFunction<T, R>): UnaryFunction<T, R>
+export function filter<T, R>(condition: UnaryFunction<T, R>): UnaryFunction<T, R>;
+export function first<T>(): UnaryFunction<T, T>;
+export function timeout<T>(time: number): UnaryFunction<T, T>;
+export function keepAlive<T>(time: number): UnaryFunction<T, T>;
+export function retry<T>(count: number): UnaryFunction<T, T>;
 export function retry<T>(options: {
   count?:           number
   delay?:           number
   resetOnSuccess?:  boolean
-}): Observable<T>;
-export function onUnsubscribe<T>(callback?: () => void): Observable<T>
+}): UnaryFunction<T, T>;
+export function onUnsubscribe<T>(callback?: () => void): UnaryFunction<T, T>;
