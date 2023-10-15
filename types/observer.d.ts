add1eea3c4639447c84be15de509921eb7755e63
Thiago Fernandes Leal <thiagofernandesleal@yahoo.com.br>
2023-10-14 23:19:01 -0300

    fix (types) : Subject definition

--- types/observer.d.ts
+++ types/observer.d.ts
@@ -1,14 +1,14 @@
 export class Observer<T> {
 	constructor(
     next: (value: T) => void | Promise<void>,
-    error: (value: any) => void | Promise<void>,
+    error: (value: unknown) => void | Promise<void>,
     complete: () => void | Promise<void>
   );
 
 	setUnsubscribe(unsubscribe: () => void | Promise<void>): void;
 
 	next(value: T): void;
-	error(err: any): void;
+	error(err: unknown): void;
 	complete(): void;
 
 	unsubscribe(): void;
--- types/subject.d.ts
+++ types/subject.d.ts
@@ -3,6 +3,10 @@ import { Observer } from "./observer";
 
 declare class BaseSubject<T> extends Observable<T> {
 	constructor(func?: (subject: Observer<T>) => ValueOrPromise<void | (() => void)>);
+
+	next(value: T): void;
+	error(err: unknown): void;
+	complete(): void;
 }
 
 export class Subject<T> extends BaseSubject<T> {
